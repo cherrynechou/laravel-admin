2b654fa97aa77a52b9d040d84046e51a679b9fb6
cherrynechou <iboy_2008@163.com>
2024-03-25 16:11:53 +0800

    'wip'

--- src/Http/Controllers/UploadController.php
+++ src/Http/Controllers/UploadController.php
@@ -5,6 +5,7 @@ namespace CherryneChou\Admin\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Str;
 use CherryneChou\Admin\Traits\HasUploadedFile;
+use CherryneChou\Admin\Traits\RestfulResponse;
 
 class UploadController extends Controller
 {
