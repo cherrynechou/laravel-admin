c6556436b8a66bfbe7d7061df5b908fe26523a31
cherrynechou <iboy_2008@163.com>
2026-02-04 14:16:49 +0800

    wip

--- resources/lang/zh_CN/menu.php
+++ resources/lang/zh_CN/menu.php
@@ -2,10 +2,10 @@
 
 return [
   'dashboard' 			        => 'Dashboard',
-  'dashboard.analysis' 	        => '分析',
-  'admin' 				        => '系统',
+  'dashboard.analysis' 	    => '分析',
+  'admin' 				          => '系统',
   'admin.user' 			        => '管理员',
   'admin.role'			        => '角色',
-  'admin.permission' 	        => '权限',
+  'admin.permission' 	      => '权限',
   'admin.menu' 			        => '菜单'
 ];
